02b083dbfe48a80d27a94e0d677a6bf76f93bd37
Matthew Wei <matthewwei35@gmail.com>
2020-10-16 22:57:46 -0700

    Added random() and randomRange()

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 AOS.init();
+
+const display = document.querySelector('#display')
+const generateButton = document.querySelector("#generate")
+const minimumInput = document.querySelector('#minimum')
+const maximumInput = document.querySelector('#maximum')
+
+function random(range) {
+    return Math.floor(Math.random() * range)
+}
+
+function randomRange() {
+    const minimumValue = parseInt(minimumInput.value)
+    const maximumValue = parseInt(maximumInput.value)
+
+    let randomNumber = random(maximumValue - minimumValue) + minimumValue
+
+    display.innerHTML = randomNumber
+}
+
+generateButton.addEventListener("click", randomRange)
